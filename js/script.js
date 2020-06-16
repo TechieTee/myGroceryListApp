d4eca52109e69ce5b0b75f8a1fc435ecbfc1532e
TechieTee <mailtomodupe@gmail.com>
2020-06-16 06:52:32 -0700

    my todolist app

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var list = [];
+document.getElementById('oneTask').innerHTML
+function clear() {
+  document.getElementById('form').reset();
+  
+}
+function del() {
+
+  document.getElementById('List').innerHTML = "";
+  document.getElementById('delete').style.display = "none";
+
+ 
+// var del = document.getElementById('delete');
+//  var i;
+//  for (var i = 0; i < list.length; i++) {
+//   document.getElementById('list').style.display = "none";
+//  }
+  
+}
+
+function add(){
+ var newtask = document.getElementById('newTask').value;
+ 
+ list.push(newtask);
+  document.getElementById('List').innerHTML =  "<li>" + list + "</li>";
+  document.getElementById('delete').style.display = "block";
+  clear();
+}
+
+
+
